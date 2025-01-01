557152a79ea9445cad21c8d595ec154bab578a13
AbdelmoneamIsmael <136120129+AbdelmoneamIsmael@users.noreply.github.com>
2025-01-01 13:10:28 +0200

    Update country_code_picker.dart

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -229,7 +229,7 @@ class CountryCodePickerState extends State<CountryCodePicker> {
                   ),
                 ),
               if (widget.showDropDownButton)
-                dropDownWidget ??
+               widget. dropDownWidget ??
                     Flexible(
                       flex: widget.alignLeft ? 0 : 1,
                       fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
